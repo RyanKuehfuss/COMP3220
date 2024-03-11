2ca89be8b36d9fedf5f3bcf48156b5ad6092b6b5
alijs123 <141453441+alijs123@users.noreply.github.com>
2024-03-11 15:01:56 -0400

    Update app.java
    added scanner functionality for user information

--- demo/src/main/java/phase2/app.java
+++ demo/src/main/java/phase2/app.java
@@ -1,10 +1,23 @@
 package phase2;
+import java.util.Scanner;
+
 
 public class app {
     // command line application to test code and database
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Please enter a username:");
+        String username = scanner.nextLine();
+        
+        System.out.println("Please enter your email:");
+        String email = scanner.nextLine();
+        
+        System.out.println("Please enter a password:");
+        String password = scanner.nextLine();
+       
         UserSignUp userSignUp = new UserSignUp();
-        boolean registrationStatus = userSignUp.registerUser("username", "user@example.com", "password");
+        boolean registrationStatus = userSignUp.registerUser(username, email, password);
 
         if (registrationStatus) {
             System.out.println("User registered successfully!");
