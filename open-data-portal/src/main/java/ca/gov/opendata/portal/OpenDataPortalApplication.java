af1b1cebcc16493e70df4766f72682b3a365cc01
RyanKuehfuss <127149364+RyanKuehfuss@users.noreply.github.com>
2024-03-19 18:46:10 -0400

    Update OpenDataPortalApplication.java

--- open-data-portal/src/main/java/ca/gov/opendata/portal/OpenDataPortalApplication.java
+++ open-data-portal/src/main/java/ca/gov/opendata/portal/OpenDataPortalApplication.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -58,6 +60,7 @@ public class OpenDataPortalApplication {
                         System.out.println("User registration failed! Username or email might already be in use.");
                     }
                 }
+                break;
 			// User selected Login option
 			case 2:
 				UserAuthenticator authenticator = new UserAuthenticator();
@@ -82,22 +85,80 @@ public class OpenDataPortalApplication {
                 if(loginComplete) {
                 	boolean signedIn = true;
                 	int dataEditChoice = -1;
+                	String apiURL = "http://localhost:8080/api/data";
                 	ResponseEntity<Data> responseEntity;
+                	RestTemplate restTemplate = new RestTemplate();
+                	Data dataset;
+                	String datasetName;
+        			String datasetDescription;
+        			String datasetID;
+        			String datasetFilePath;
+        			String id;
                 	
                 	while(signedIn) {                		
-                		System.out.printf("What would you like to do?\n1. Create a dataset\n2. Get all datasets (To be added in next iteration)\n3. Get single dataset by ID (To be added in next iteration)\n4. Update a dataset (To be added in next iteration)\n5. Delete a dataset (To be added in next iteration)\n6. Logout\n> ");
+                		System.out.printf("What would you like to do?\n1. Create a dataset\n2. Get all datasets\n3. Get single dataset by ID\n4. Update a dataset\n5. Delete a dataset\n6. Logout\n> ");
                 		dataEditChoice = scanner.nextInt();
                 		scanner.nextLine();
                 		
                 		switch(dataEditChoice) {
                 		// User selects create dataset
                 		case 1:
-                			String datasetName;
-                			String datasetDescription;
-                			String datasetID;
-                			String datasetFilePath;
-                			String apiURL = "http://localhost:8080/api/data";
-                			RestTemplate restTemplate = new RestTemplate();
+                			System.out.printf("Enter the name of the dataset: ");
+                			datasetName = scanner.nextLine();
+                			
+                			System.out.printf("Enter the description of the dataset: ");
+                			datasetDescription = scanner.nextLine();
+                			
+                			System.out.printf("Enter the file path of the dataset: ");
+                			datasetFilePath = scanner.nextLine();
+                			
+                			System.out.printf("Enter the ID of the dataset: ");
+                			datasetID = scanner.nextLine();
+                			
+                			dataset = new Data(datasetName, datasetDescription, datasetFilePath, datasetID, username);
+                			
+                			responseEntity = restTemplate.postForEntity(apiURL, dataset, Data.class);
+                			
+                			if(responseEntity.getStatusCode().is2xxSuccessful()) {
+                				System.out.printf("The dataset was successfully created\n");
+                			}else {
+                				System.out.printf("Error. Dataset not successfully created.\n");
+                			}
+                			break;
+                		// User selects get all datasets
+                		case 2:
+                			ResponseEntity<Data[]> getAllResponse = restTemplate.getForEntity(apiURL, Data[].class);
+                	        Data[] dataList = getAllResponse.getBody();
+                	        System.out.printf("All Data:\n");
+                	        for (Data d : dataList) {
+                	            System.out.println(d);
+                	        }
+                	        
+                	        if(getAllResponse.getStatusCode().is2xxSuccessful()) {
+                				System.out.printf("All datasets successfully retrieved\n");
+                			}else {
+                				System.out.printf("Error. Datasets not successfully retrieved.\n");
+                			}
+                	        break;
+                		// User selects get dataset by ID
+                		case 3:
+                			System.out.printf("Enter the ID of the dataset you wish to view: ");
+                			id = scanner.next();
+                			
+                	        responseEntity = restTemplate.getForEntity(apiURL + "/" + id, Data.class);
+                	        System.out.printf("Get Data Response: " + responseEntity.getBody());
+                	        
+                	        if(responseEntity.getStatusCode().is2xxSuccessful()) {
+                				System.out.printf("The dataset was successfully retrieved\n");
+                			}else {
+                				System.out.printf("Error. Dataset not successfully retrieved.\n");
+                			}
+                			break;
+                		// User selects update a dataset
+                		case 4:
+                			System.out.printf("Enter the ID of the dataset you wish to update: ");
+                			id = scanner.next();
+                			scanner.nextLine();
                 			
                 			System.out.printf("Enter the name of the dataset: ");
                 			datasetName = scanner.nextLine();
@@ -111,27 +172,34 @@ public class OpenDataPortalApplication {
                 			System.out.printf("Enter the ID of the dataset: ");
                 			datasetID = scanner.nextLine();
                 			
-                			Data dataset = new Data(datasetName, datasetDescription, datasetFilePath, datasetID, username);
+                			dataset = new Data(datasetName, datasetDescription, datasetFilePath, datasetID, username);
                 			
                 			responseEntity = restTemplate.postForEntity(apiURL, dataset, Data.class);
                 			
-                			if(responseEntity.getStatusCode().is2xxSuccessful()) {
-                				System.out.printf("The dataset was successfully created\n");
+                	        responseEntity = restTemplate.exchange(apiURL + "/" + id, HttpMethod.PUT, new HttpEntity<>(dataset), Data.class);
+                	        System.out.println("Update Data Response: " + responseEntity.getBody());
+                			
+                	        if(responseEntity.getStatusCode().is2xxSuccessful()) {
+                				System.out.printf("The dataset was successfully updated\n");
                 			}else {
-                				System.out.printf("Error. Dataset not successfully created.\n");
+                				System.out.printf("Error. Dataset not successfully updated.\n");
                 			}
-                		// User selects get all datasets
-                		case 2:
-                		
-                		// User selects get dataset by ID
-                		case 3:
-                			
-                		// User selects update a dataset
-                		case 4:
-                			
+                	        break;
                 		// User selects delete a dataset
                 		case 5:
+                			System.out.printf("Enter the ID of the dataset you wish to delete: ");
+                			id = scanner.next();
+                			scanner.nextLine();
                 			
+                			ResponseEntity<?> deleteResponse = restTemplate.exchange(apiURL + "/" + id, HttpMethod.DELETE, null, Object.class);
+                	        System.out.println("Delete Data Response: " + deleteResponse.getStatusCode());
+                	        
+                	        if(deleteResponse.getStatusCode().is2xxSuccessful()) {
+                				System.out.printf("The dataset was successfully deleted\n");
+                			}else {
+                				System.out.printf("Error. Dataset not successfully deleted.\n");
+                			}
+                			break;
                 		// User selects logout
                 		case 6:
                 			System.out.println("Logging out.");
